0459ac0839956d944c63edcaa80dd9cc3680a393
Eugene Datsky <eugene@datsky.ru>
2017-07-05 15:40:46 +0300

    chore: Re-order sections in test.js

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 module.exports = {
-  "rules": {
-    "no-unused-expressions": 0,
-    "bdd/focus": 2,
-    "bdd/exclude": 0
-  },
   "env": {
     "mocha": true
   },
+  "plugins": [
+    "bdd"
+  ],
   "globals": {
     "expect": false,
     "sinon": false,
     "should": false
   },
-  "plugins": [
-    "bdd"
-  ]
+  "rules": {
+    "no-unused-expressions": 0,
+    "bdd/focus": 2,
+    "bdd/exclude": 0
+  }
 };
