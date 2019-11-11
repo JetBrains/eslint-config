2da498bb695f1e66418e1e6c1436658108988d64
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2019-11-11 23:12:24 +0100

    feat(import): add rules

--- es6.js
+++ es6.js
@@ -42,6 +42,8 @@ module.exports = {
     'import/extensions': [error, 'always', {js: 'never'}],
     'import/order': [error, {'newlines-between': 'always-and-inside-groups'}],
     'import/newline-after-import': error,
-    'import/no-named-default': error
+    'import/no-named-default': error,
+    'import/no-unused-modules': [error, {unusedExports: true}],
+    'import/no-useless-path-segments': [error, {noUselessIndex: true}]
   }
 };
